322a715ca149df16dd39cd2af7fc8bb234f57232
= <=>
2020-08-05 21:12:42 +0200

    REMOVE LARAVEL SOCIALITE

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,13 +17,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            // add your listeners (aka providers) here
-            'SocialiteProviders\\Facebook\\FacebookExtendSocialite@handle',
-            'SocialiteProviders\\LinkedIn\\LinkedInExtendSocialite@handle',
-            'SocialiteProviders\\Google\\GoogleExtendSocialite@handle',
-            'SocialiteProviders\\Twitter\\TwitterExtendSocialite@handle',
-            ],
+
     ];
 
     /**
